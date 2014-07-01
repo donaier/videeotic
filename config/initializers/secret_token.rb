09d4247c62163c25b2a5696cb15df3098825f150
Donat Baier <donat.baier@gmail.com>
2014-07-01 20:21:42 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videeotic::Application.config.secret_key_base = 'fd445077f89ab717595c0696a8dc6847caa835485518cd7fb3ff60aecc19ad9521b0f5b87b5c62ac5cb672d439ee8334aa50b176e52b208c1f87c233b5e6e3a7'
