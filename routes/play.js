84928bf047a085b3d911b8cb82ae0f4facd6b998
Donat Baier <donat.baier@gmail.com>
2014-09-08 18:11:07 +0200

    dummy commit

--- routes/play.js
+++ routes/play.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('play/index', { title: 'Play dem file!', path: req.query.path });
+  res.render('play/index', { title: 'Play file!', path: req.query.path });
 });
 
 module.exports = router;
